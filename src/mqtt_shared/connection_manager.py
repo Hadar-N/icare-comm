98f957ca44edb526a761d38e8a5dbbe90f8495e9
Hadar-N <hadarnv1@gmail.com>
2025-04-24 19:59:04 +0800

    clearout data saving

--- src/mqtt_shared/connection_manager.py
+++ src/mqtt_shared/connection_manager.py
@@ -25,10 +25,6 @@ class ConnectionManager:
 
         self.__initialized = False
 
-    def get_current_game_status(self) -> GAME_STATUS: return self.__game_status
-    
-    def get_matched_list(self) -> list: return self.__matched_words
-
     @classmethod
     def get_instance(cls):
         if not cls._instance:
@@ -45,9 +41,7 @@ class ConnectionManager:
         cls.__role = role
         cls.__outer_handle_messages = handle_message
 
-        cls.__matched_words = []
         cls.__read_queue = {}
-        cls.__game_status = GAME_STATUS.HALTED
 
         cls.__mqtt_instance = MQTTBaseClass(
             base,
@@ -74,20 +68,11 @@ class ConnectionManager:
         body = BodyForTopic(topic, msg)
         self.__mqtt_instance.publish_message(topic, body)
 
-    def __update_status(self, new_status: GAME_STATUS, timestamp: float = None):
-        self.__game_status = new_status
-        if self.__game_status == GAME_STATUS.ACTIVE:
-            self.__read_queue = {}
-            self.__matched_words = []
-
     def __handle_message(self, topic:str, data: BodyObject):
         asdict = data.bodyToDict()
-        if topic == Topics.STATE:
-            self.__update_status(data.state)
-        elif Topics.is_word_state(topic):
+        if Topics.is_word_state(topic):
             for d_id in self.__read_queue:
                 self.__read_queue[d_id].put(asdict)
-            if data.type == MQTT_DATA_ACTIONS.MATCHED: self.__matched_words.append(asdict)
 
         if self.__outer_handle_messages: self.__outer_handle_messages(topic = topic, data = asdict)
     
